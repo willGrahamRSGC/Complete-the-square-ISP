c6914d6031593d0f956e7e5b05bb816199fbdcf5
Will <student@MacBook-Pro-0690-Graham.local>
2016-01-12 10:09:59 -0500

    errors with math
    i have been having errors with my math that i don’t understand working
    on fixing.

---
+++ complete the square isp.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+// value holds for "b"
+var b4:Double = 0
+var b2:Double = 0
+var b3:Double = 0
+// instert "a" value
+var a:Double = 0
+// insert "b" value
+var b:Double = 0
+// insert "c" value
+var c:Double = 0
+
+//if (a = 0) {
+//a +=  a + 1
+//}
+// mathimatically calculating "b" value
+ (b / a) = b2
+
+(b2 / 2) * b2 = b3
