c2bd9baa6e0cd2d5634a8e35c4b603c313875881
Will <student@MacBook-Pro-0690-Graham.local>
2016-01-17 08:31:07 -0500

    making changes to fix small errors

--- complete the square isp.playground/Contents.swift
+++ complete the square isp.playground/Contents.swift
@@ -5,17 +5,24 @@ import Cocoa
 var b4:Double = 0
 var b2:Double = 0
 var b3:Double = 0
+var fb: DOuble = 0
 // instert "a" value
-var a:Double = 0
+var a:Double = 10
 // insert "b" value
-var b:Double = 0
+var b:Double = 100
 // insert "c" value
-var c:Double = 0
+var c:Double = 30
 
 //if (a = 0) {
 //a +=  a + 1
 //}
 // mathimatically calculating "b" value
- (b / a) = b2
+b2 = (b / a)
 
-(b2 / 2) * b2 = b3
+b3 = (b2 / 2) * b2
+b4 = (b3*a)
+
+// calculate ending c
+ c = c + b4
+
+//calculate final b 
