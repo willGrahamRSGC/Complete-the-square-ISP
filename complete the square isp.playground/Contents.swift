f696d46f80e4921afbd184a163cacadadadcd49a
Will <student@MacBook-Pro-0690-Graham.local>
2016-01-18 09:58:22 -0500

    finished product
    whoops committed wrong program

--- complete the square isp.playground/Contents.swift
+++ complete the square isp.playground/Contents.swift
@@ -5,9 +5,9 @@ import Cocoa
 var b4:Double = 0
 var b2:Double = 0
 var b3:Double = 0
-var fb: DOuble = 0
+var fb:Double = 0
 // instert "a" value
-var a:Double = 10
+var a:Double = 13
 // insert "b" value
 var b:Double = 100
 // insert "c" value
@@ -16,13 +16,26 @@ var c:Double = 30
 //if (a = 0) {
 //a +=  a + 1
 //}
-// mathimatically calculating "b" value
+
+// mathimatically calculating "b" value (less accurasy)
+//b2 = (b % a)
+//
+//b3 = (b2 % 2) * b2
+//b4 = (b3*a)
+
+
+// mathimatically calculating "b" value ( very precise )
 b2 = (b / a)
 
 b3 = (b2 / 2) * b2
 b4 = (b3*a)
 
-// calculate ending c
- c = c + b4
+// final answer!!
 
-//calculate final b 
+// final a
+a
+//calculate final b
+fb = b2/2
+
+// calculate final c
+c = c + b4
